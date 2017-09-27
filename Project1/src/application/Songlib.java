1c2dc33d91d06e34eab2dfb79607efc338bf02aa
Neil Malu <Neil@nbp-148-76.nbp.ruw.rutgers.edu>
2017-09-27 17:11:59 -0400

    Created controller

--- Project1/src/application/Songlib.java
+++ Project1/src/application/Songlib.java
@@ -1,19 +1,20 @@
 package application;
 	
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
 
 
-public class Main extends Application {
+public class Songlib extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			primaryStage.setScene(scene);
+			Parent root = FXMLLoader.load(getClass().getResource("/Scene.fxml"));
+			primaryStage.setTitle("My Application");
+			primaryStage.setScene(new Scene(root));
+			primaryStage.setResizable(false);
 			primaryStage.show();
 		} catch(Exception e) {
 			e.printStackTrace();
@@ -24,3 +25,4 @@ public class Main extends Application {
 		launch(args);
 	}
 }
+
