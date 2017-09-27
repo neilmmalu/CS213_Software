7e818d746c1b80361215e8a95b4c89018c556d14
Neil Malu <Neil@nwk-138-85.nwk.ruw.rutgers.edu>
2017-09-27 19:25:56 -0400

    Skeleton almost done

--- Project1/src/application/Controller.java
+++ Project1/src/application/Controller.java
@@ -1,5 +1,12 @@
 package application;
 
+import java.util.ArrayList;
+
+import javafx.stage.Stage;
+
 public class Controller {
 	
+	public void start(Stage primaryStage, ArrayList<Song> masterList) {
+		
+	}
 }
--- Project1/src/application/Songlib.java
+++ Project1/src/application/Songlib.java
@@ -1,19 +1,29 @@
 package application;
 	
+import java.util.ArrayList;
+
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
 
 
 public class Songlib extends Application {
+	
+	static ArrayList<Song> masterList;
+	AnchorPane root;
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			Parent root = FXMLLoader.load(getClass().getResource("/Scene.fxml"));
-			primaryStage.setTitle("My Application");
-			primaryStage.setScene(new Scene(root));
+			FXMLLoader loader = new FXMLLoader();
+			loader.setLocation(getClass().getResource("/Scene.fxml"));
+			
+			root = (AnchorPane) loader.load();
+			Controller listController = loader.getController();
+			listController.start(primaryStage, masterList);
+			Scene scene = new Scene(root);
+			primaryStage.setScene(scene);
 			primaryStage.setResizable(false);
 			primaryStage.show();
 		} catch(Exception e) {
@@ -22,6 +32,8 @@ public class Songlib extends Application {
 	}
 	
 	public static void main(String[] args) {
+		masterList = new ArrayList<Song>();
+		
 		launch(args);
 	}
 }
