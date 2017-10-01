3807349ece4fdfbc37804c48f0f61672fc51dea8
Neil Malu <Neil@Neils-MBP.fios-router.home>
2017-10-01 01:04:30 -0400

    Add event

--- Project1/src/application/Controller.java
+++ Project1/src/application/Controller.java
@@ -2,11 +2,43 @@ package application;
 
 import java.util.ArrayList;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+import javafx.scene.control.Alert.AlertType;
 import javafx.stage.Stage;
 
 public class Controller {
 	
+	@FXML ListView<String> SongList;
+	@FXML TextArea SongDetails;
+	@FXML Button AddSong;
+	@FXML Button DeleteSong;
+	@FXML Button EditSong;
+	@FXML TextField SongNameAdd;
+	@FXML TextField SongArtistAdd;
+	@FXML TextField SongAlbumAdd;
+	@FXML TextField SongYearAdd;
+	
 	public void start(Stage primaryStage, ArrayList<Song> masterList) {
 		
 	}
+	
+	public void add_song(ActionEvent event) {
+		if(SongNameAdd.getText().equals("") || SongArtistAdd.getText().equals("")) {
+			Alert alert = new Alert(AlertType.ERROR);
+			alert.setTitle("Invalid Input");
+			alert.setContentText("Song name and artist required. Please try again");
+			alert.showAndWait();
+		}
+	}
+	
+	public void delete_song(ActionEvent event) {
+			
+	}
+	
+	public void edit_song(ActionEvent event) {
+		
+	}
+	
 }
